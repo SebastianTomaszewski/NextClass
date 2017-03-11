a911f8b7ad3a2c2f2d358e43d49673629ad5915b
Sebastian Tomaszewski <sebastian.tomaszewski.st@outlook.com>
2017-03-11 16:10:13 +0100

    Add to BlackList

--- NextClass/Traceability.cs
+++ NextClass/Traceability.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using NextClass.Model;
-using static System.Int32;
+using static System.Data.SqlDbType;
+using static System.String;
 
 namespace NextClass
 {
@@ -13,7 +14,6 @@ namespace NextClass
 
         private SqlConnection _conn;
         private MsSql _msSql;
-        // ReSharper disable once FieldCanBeMadeReadOnly.Local
         private string _connStr;
 
 
@@ -24,7 +24,6 @@ namespace NextClass
             _connStr = connectionStringModel.ConnectionString;
             _msSql = new MsSql(connectionStringModel);
         }
-
         public Traceability(string connectionString)
         {
             _connStr = connectionString;
@@ -32,46 +31,6 @@ namespace NextClass
         }
 
         #endregion
-
-        //EXEC	[dbo].[ASK_SN]	@LINE = N'JUNIOR',	@OP = 750, @PN = N'26111047',	@SN = N'26111047062173033700'
-        public object Ask_SN(string line,string op, string pn, string sn)
-        {
-
-            var paramList = new List<SqlParameter>
-            {
-                new SqlParameter("@LINE", SqlDbType.VarChar, 50) {Value = line, IsNullable = false},
-                new SqlParameter("@OP", SqlDbType.Int) {Value = op,IsNullable = false},
-                new SqlParameter("@PN", SqlDbType.VarChar, 50) {Value = pn,IsNullable = true},
-                new SqlParameter("@SN", SqlDbType.VarChar, 50) {Value = sn,IsNullable = false}
-            };
-
-
-            //@EXTNAME000 varchar(100) = NULL,
-            //@EXTNAME010 varchar(100) = NULL,
-            //@EXTNAME020 varchar(100) = NULL,
-            //@EXTNAME030 varchar(100) = NULL,
-            //@EXTNAME040 varchar(100) = NULL,
-            //@EXTNAME050 varchar(100) = NULL,
-            //@EXTNAME060 varchar(100) = NULL,
-            //@EXTNAME070 varchar(100) = NULL,
-            //@EXTNAME080 varchar(100) = NULL,
-            //@EXTNAME090 varchar(100) = NULL,
-            //@EXTNAME100 varchar(100) = NULL,
-            //@EXTNAME110 varchar(100) = NULL,
-            //@EXTNAME120 varchar(100) = NULL,
-            //@EXTNAME130 varchar(100) = NULL,
-            //@EXTNAME140 varchar(100) = NULL,
-            //@EXTNAME150 varchar(100) = NULL,
-            //@EXTNAME160 varchar(100) = NULL,
-            //@EXTNAME170 varchar(100) = NULL,
-            //@EXTNAME180 varchar(100) = NULL,
-            //@EXTNAME190 varchar(100) = NULL
-
- 
-           
-            return _msSql.ExecuteReader("[dbo].[ASK_SN]", CommandType.StoredProcedure, paramList.ToArray());
-        }
-
 
         public bool IsConnection()
         {
@@ -89,6 +48,60 @@ namespace NextClass
             }
         }
 
+        //EXEC	[dbo].[ASK_SN]	@LINE = N'JUNIOR',	@OP = 750, @PN = N'26111047',	@SN = N'26111047062173033700'
+        public SqlDataReader Ask_SN(string line, string op, string pn, string sn)
+        {
 
+            var paramList = new List<SqlParameter>
+            {
+                new SqlParameter("@LINE", VarChar, 50) {Value = line, IsNullable = false},
+                new SqlParameter("@OP", Int) {Value = op, IsNullable = false},
+                new SqlParameter("@PN", VarChar, 50) {Value = pn, IsNullable = true},
+                new SqlParameter("@SN", VarChar, 50) {Value = sn, IsNullable = false}
+            };
+
+            return _msSql.ExecuteReader("[dbo].[ASK_SN]", CommandType.StoredProcedure, paramList.ToArray());
+        }
+        public SqlDataReader Ask_SN(string line, string op, string pn, string sn, string[] parm )
+        {
+            if (IsNullOrWhiteSpace(line)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(line));
+            if (IsNullOrWhiteSpace(op))   throw new ArgumentException("Value cannot be null or whitespace.", nameof(op));
+            if (IsNullOrWhiteSpace(sn))   throw new ArgumentException("Value cannot be null or whitespace.", nameof(sn));
+            if (parm.Length == 0)         throw new ArgumentException("Value cannot be an empty collection.", nameof(parm));
+            if (pn == null)               throw new ArgumentNullException(nameof(pn));
+
+            var list = new List<SqlParameter>
+            {
+                new SqlParameter("@LINE", VarChar, 50) {Value = line, IsNullable = false},
+                new SqlParameter("@OP", Int) {Value = op, IsNullable = false},
+                new SqlParameter("@PN", VarChar, 50) {Value = pn, IsNullable = true},
+                new SqlParameter("@SN", VarChar, 50) {Value = sn, IsNullable = false}
+            };
+
+            if (parm.Length != 0) list.AddRange(parm.Select((t, i) => new SqlParameter($"@EXTNAME{i:D2}0", VarChar, 100) {Value = t, IsNullable = true}));
+
+            return _msSql.ExecuteReader("[dbo].[ASK_SN]", CommandType.StoredProcedure, list.ToArray());
+        }
+
+        public int ADD_TO_BLACKLIST(string sn, string code, string remarks)
+        {//ToDo: dopytać czy do blacklisty można dodać osobę blokującą?
+            if (IsNullOrWhiteSpace(sn)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(sn));
+            if (IsNullOrWhiteSpace(code))throw new ArgumentException("Value cannot be null or whitespace.", nameof(code));
+            if (IsNullOrWhiteSpace(remarks))throw new ArgumentException("Value cannot be null or whitespace.", nameof(remarks));
+            
+
+            var list = new List<SqlParameter>
+            {
+                new SqlParameter("@LINE", VarChar, 100) {Value = sn, IsNullable = false},
+                new SqlParameter("@PN", VarChar, 50) {Value = code, IsNullable = false},
+                new SqlParameter("@SN", VarChar, 500) {Value = remarks, IsNullable = false}
+            };
+
+         //   EXEC[dbo].[ADD_TO_BLACKLIST] @sn = N'26111047062173033600',@code = N'test',@remarks = N'testtesttest'
+
+                return _msSql.ExecuteNonQuery("[dbo].[ADD_TO_BLACKLIST]", CommandType.StoredProcedure, list.ToArray());
+
+
+        }
     }
 }
